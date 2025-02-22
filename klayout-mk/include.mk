b395b68eb01855f26209e3e62594a4167489a6f6
Huwan Peng <hwpeng20@gmail.com>
2025-02-21 16:29:58 -0800

    Update klayout include.mk to 2025

--- klayout-mk/include.mk
+++ klayout-mk/include.mk
@@ -3,15 +3,14 @@
 
 REAL_OBJ_DIR = $(realpath $(OBJ_DIR))
 
-RUBYLIB := /home/projects/ee477.2023wtr/cad/klayout/ruby/lib64/ruby
-RUBYLIB := $(RUBYLIB):/home/projects/ee477.2023wtr/cad/klayout/ruby/lib64/rubygems
-RUBYLIB := $(RUBYLIB):/home/projects/ee477.2023wtr/cad/klayout/ruby/share/ruby
+RUBYLIB := /home/projects/ee477.2025wtr/cad/klayout/bin-release/ruby/lib64/ruby
+RUBYLIB := $(RUBYLIB):/home/projects/ee477.2025wtr/cad/klayout/bin-release/ruby/lib64/rubygems
+RUBYLIB := $(RUBYLIB):/home/projects/ee477.2025wtr/cad/klayout/bin-release/ruby/share/ruby
+export LD_LIBRARY_PATH := /home/projects/ee477.2025wtr/cad/klayout/bin-release:$(LD_LIBRARY_PATH)
 
-KLAYOUT := RUBYLIB=$(RUBYLIB) /home/projects/ee477.2023wtr/cad/klayout/klayout
+KLAYOUT := RUBYLIB=$(RUBYLIB) /home/projects/ee477.2025wtr/cad/klayout/bin-release/klayout
 KLAYOUT_GDS_FILE := $(firstword $(wildcard $(REAL_OBJ_DIR)/par-rundir/*.gds/))
 
 # TODO: un hard code? Make hook?
 open-klayout-gds:
-	$(KLAYOUT) -l /home/projects/ee477.2023wtr/cad/pdk/sky130A/libs.tech/klayout/sky130A.lyp $(KLAYOUT_GDS_FILE)
-	
-
+  $(KLAYOUT) -l /home/projects/ee477.2025wtr/cad/pdk/sky130A/libs.tech/klayout/sky130A.lyp $(KLAYOUT_GDS_FILE)
